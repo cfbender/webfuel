528f539ef4e83f1df28d7104fa158bf9b49da9b3
Cody Bender <codyfbender@gmail.com>
2019-12-01 17:36:59 -0700

    Add initial styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,43 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from "react";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
 
 const IndexPage: NextPage = () => {
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
+    <Layout>
       <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
+        Lorem ipsum dolor amet snackwave vice PBR&B waistcoat tacos iceland
+        prism. Small batch actually fingerstache, echo park YOLO pop-up shaman
+        whatever master cleanse kombucha vegan 8-bit bushwick lyft. Ethical
+        poutine tofu mustache jianbing heirloom gastropub af fingerstache pabst
+        butcher tilde. Mustache bitters listicle, tbh you probably haven't heard
+        of them schlitz copper mug tattooed chia squid biodiesel narwhal.
+        Tousled shaman farm-to-table fingerstache enamel pin authentic. Truffaut
+        taiyaki sustainable heirloom, aesthetic 3 wolf moon iceland meditation
+        irony bitters. Cloud bread trust fund squid locavore polaroid organic
+        paleo sriracha ramps shoreditch cold-pressed direct trade lomo. Lomo
+        XOXO irony knausgaard, kitsch vape mlkshk twee distillery lumbersexual
+        forage ethical. Chartreuse leggings try-hard, shaman swag 90's mustache
+        vaporware celiac artisan gluten-free messenger bag. Church-key photo
+        booth succulents shaman before they sold out vexillologist art party
+        gastropub cronut man bun trust fund taxidermy. Hammock godard leggings
+        squid, occupy authentic tattooed. VHS literally shaman +1 air plant
+        before they sold out meggings activated charcoal. Tote bag before they
+        sold out tbh tofu fashion axe woke taiyaki mumblecore pork belly.
       </p>
+      <style jsx global>{`
+        html,
+        body,
+        #__next {
+          margin: 0 auto;
+          height: 100%;
+        }
+        p {
+          padding: 50px;
+        }
+      `}</style>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
