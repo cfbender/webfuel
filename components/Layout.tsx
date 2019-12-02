71ff4a316ee1773c17623f835447907f1ca1527f
Cody Bender <codyfbender@gmail.com>
2019-12-01 17:36:59 -0700

    Add initial styling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,42 +1,161 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { useState } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import { Lottie } from "@crello/react-lottie";
+import coffee from "../public/assets/coffee-outline.json";
 
 type Props = {
-  title?: string
-}
+  children?: JSX.Element[];
+};
 
-const Layout: React.FunctionComponent<Props> = ({
-  children,
-  title = 'This is the default title',
-}) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+const Layout: React.FunctionComponent<Props> = ({ children }) => {
+  const [playing, setPlaying]: ["playing" | "stopped", any] = useState(
+    "stopped"
+  );
 
-export default Layout
+  return (
+    <div id="layout">
+      <Head>
+        <title>WebFuel</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <div className="content">
+        <header className="header">
+          <nav>
+            <Link href="/">
+              <div
+                className="logo"
+                onMouseEnter={() => void setPlaying("playing")}
+                onMouseLeave={() => void setPlaying("stopped")}
+              >
+                <Lottie
+                  config={{
+                    animationData: coffee,
+                    autoplay: true,
+                    loop: true
+                  }}
+                  height={"80px"}
+                  width={"80px"}
+                  playingState={playing}
+                />
+                <h1 className="logo-name">WebFuel</h1>
+              </div>
+            </Link>
+
+            <div className="routes">
+              <Link href="/challenges" passHref>
+                <a className="route-link">Challenges</a>
+              </Link>{" "}
+              |
+              <Link href="/learn" passHref>
+                <a className="route-link">Learn</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        {children}
+      </div>
+      <footer>
+        <svg
+          id="Artwork"
+          className="h-8 fill-current inline"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 154.87 153.27"
+          width="25px"
+        >
+          <title>crossed-keys-bold</title>
+          <text x="-282.53" y="-211.04"></text>
+          <path
+            className="cls-1"
+            d="M398,279.86a22.18,22.18,0,1,0,18.52-37.66,22.19,22.19,0,1,0-28.72,23.94l-27,27-27.46-27.46a22.18,22.18,0,1,0-30-23.51A22.19,22.19,0,1,0,327,271.76l27.6,27.6-46.83,46.83a2.59,2.59,0,0,0,0,3.66l2.52,2.52a2.59,2.59,0,0,0,3.66,0l46.83-46.83,31.56,31.56-12,12a2.5,2.5,0,0,0,3.54,3.54l3.71-3.71,5.94,5.94-3.71,3.71a2.5,2.5,0,1,0,3.54,3.54l12-12-11-11,13.21,13.21a2.59,2.59,0,0,0,3.66,0l2.52-2.52a2.59,2.59,0,0,0,0-3.66L367,299.35l26.4-26.4A21.93,21.93,0,0,0,398,279.86Zm-74.51-18.5a0.48,0.48,0,0,0-.54.56,16.63,16.63,0,0,1-28.67,13.68,16.79,16.79,0,0,1,.49-23.09,16.58,16.58,0,0,1,13.57-4.71,0.46,0.46,0,0,0,.51-0.53,16.6,16.6,0,0,1,4.49-14,16.63,16.63,0,1,1,23.73,23.32A16.58,16.58,0,0,1,323.53,261.36Zm59.16-4.43A16.63,16.63,0,1,1,411,247a0.46,0.46,0,0,0,.53.51,16.6,16.6,0,0,1,14,4.49,16.63,16.63,0,1,1-23.32,23.73,16.58,16.58,0,0,1-4.75-13.58,0.48,0.48,0,0,0-.56-0.54A16.6,16.6,0,0,1,382.69,256.93Z"
+            transform="translate(-282.53 -211.04)"
+          ></path>
+          <path
+            className="cls-1"
+            d="M329.24,337.09l-13,13,12,12a2.5,2.5,0,1,0,3.54-3.54L328,354.83l5.94-5.94,3.71,3.71a2.5,2.5,0,1,0,3.54-3.54Z"
+            transform="translate(-282.53 -211.04)"
+          ></path>
+        </svg>
+        <span className="footer-text">
+          Created and maintained by <a href="https://cfbender.dev">cfbender</a>
+        </span>
+      </footer>
+
+      <style jsx>
+        {`
+          @import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
+          #layout {
+            margin: 0 auto;
+            display: flex;
+            height: 100%;
+            flex-direction: column;
+          }
+          .content {
+            flex: 1 0 auto;
+          }
+          nav {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          }
+          .route-link {
+            color: #e4bd54;
+          }
+          .header {
+            background-color: #19262f;
+            padding: 25px;
+            margin: 0 auto;
+          }
+          .routes {
+            display: flex;
+            min-width: 200px;
+            justify-content: space-between;
+            color: #c94437;
+          }
+          .logo {
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+          }
+
+          .logo-name {
+            color: #e4bd54;
+            font-family: "Montserrat", sans-serif;
+            font-size: 32px;
+            margin: 0;
+            margin-left: 10px;
+          }
+
+          .cls-1 {
+            fill: #e4bd54;
+            stroke: #e4bd54;
+            stroke-miterlimit: 10;
+            stroke-width: 3px;
+            max-width: 25px;
+          }
+
+          footer {
+            background-color: #19262f;
+            padding: 15px;
+            flex-shrink: 0;
+            align-items: center;
+          }
+
+          .footer-text {
+            color: #c94437;
+            margin-left: 10px;
+          }
+
+          a {
+            color: #5abdae;
+            text-decoration: none;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default Layout;
